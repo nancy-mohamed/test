ee3b87a258d3ca11b7adf8ddf599b72a6a4ac0f3
nancy-mohamed <draz.nona40@gmail.com>
2025-04-28 14:03:20 +0300

    start

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-$(document).ready(function () {
-            
-          // Toggle .sub_menu when clicking .menu_mobile li a
-          $('.menu_mobile li a').on('click', function (e) {
-            e.preventDefault(); // Prevent default anchor behavior
-        
-            // Toggle the nearest .sub_menu within the same list item
-            $(this).siblings('.sub_menu').slideToggle();
-        
-            // Optionally close other open submenus (if required)
-            // $('.menu_mobile .sub_menu').not($(this).siblings('.sub_menu')).slideUp();
-          });
-                
-})
+document.addEventListener('DOMContentLoaded', function () {
+  const menuLinks = document.querySelectorAll('.menu_mobile li a');
+
+  menuLinks.forEach(function (link) {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      const subMenu = this.parentElement.querySelector('.sub_menu');
+      if (subMenu) {
+        subMenu.classList.toggle('open');
+      }
+    });
+  });
+});
